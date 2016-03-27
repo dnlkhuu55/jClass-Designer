928ed3950d1e3186b1ea8cdad76c516f235ad588
exleader75 <danielkhuu@yahoo.com>
2016-03-27 19:58:59 -0400

    Message: Completed most of the UI.

--- jClassDesigner/src/jcd/gui/Workspace.java
+++ jClassDesigner/src/jcd/gui/Workspace.java
@@ -58,6 +58,11 @@ import static af.settings.AppStartupConstants.PATH_IMAGES;
 import static af.settings.AppStartupConstants.PATH_WORK;
 import af.ui.AppMessageDialogSingleton;
 import af.ui.AppYesNoCancelDialogSingleton;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
 
 /**
  * This class serves as the workspace component for this application, providing
@@ -76,17 +81,34 @@ public class Workspace extends AppWorkspaceComponent {
     AppGUI gui;
 
     BorderPane entirePane;
-    Pane leftPane;
-    Pane rightPane;
+    ScrollPane leftPane;
+    VBox rightPane;
     HBox classPane;
     HBox packagePane;
-    VBox variables;
-    VBox methods;
-    Button select = new Button();
-    Button variableadd = new Button();
-    Button variableremove = new Button();
-    Button methodAdd = new Button();
-    Button methodRemove = new Button();
+    HBox parentPane;
+    HBox variablePane;
+    StackPane variableTablePane;
+    HBox methodPane;
+    StackPane methodTablePane;
+    Button photoButton = new Button();
+    Button codeButton = new Button();
+    Button selectionButton = new Button();
+    Button resizeButton = new Button();
+    Button addClassButton = new Button();
+    Button addInterfaceButton = new Button();
+    Button removeButton = new Button();
+    Button undoButton = new Button();
+    Button redoButton = new Button();
+    Button zoominButton = new Button();
+    Button zoomoutButton = new Button();
+    Button addButton = new Button();
+    Button addMButton = new Button();
+    Button minusButton = new Button();
+    Button minusMButton = new Button();
+    TextField dummy = new TextField();
+    TextField dummyData = new TextField();
+    TableView table1 = new TableView();
+    TableView table2 = new TableView();
     Button selectedButton;
     Shape currentShape = null;
     
@@ -114,75 +136,127 @@ public class Workspace extends AppWorkspaceComponent {
         
 	app = initApp;
 	gui = app.getGUI();
-	PropertiesManager propsSingleton = PropertiesManager.getPropertiesManager();
         DataManager expo = (DataManager) app.getDataComponent();
 
 	workspace = new BorderPane();
-	leftPane = new VBox();  
-        rightPane = new Pane();
-        rightPane.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-        expo.setBackgroundColor(Color.WHITE);
+	leftPane = new ScrollPane();  
+        rightPane = new VBox();
+        leftPane.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        //expo.setBackgroundColor(Color.WHITE);
         
-        
-        
+  
         FlowPane fileToolbarPane = gui.getFileToolbarPane();
         
-        select = initChildButton(fileToolbarPane, PropertyType.SELECTION_ICON.toString(), PropertyType.SELECTION_TOOLTIP.toString(), false);
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        photoButton = initChildButton(fileToolbarPane, PropertyType.PHOTO_ICON.toString(), PropertyType.PHOTO_TOOLTIP.toString(), false);
+        codeButton = initChildButton(fileToolbarPane, PropertyType.CODE_ICON.toString(), PropertyType.CODE_TOOLTIP.toString(), false);
+        selectionButton = initChildButton(fileToolbarPane, PropertyType.SELECTION_ICON.toString(), PropertyType.SELECTION_TOOLTIP.toString(), false);
+        resizeButton = initChildButton(fileToolbarPane, PropertyType.RESIZE_ICON.toString(), PropertyType.RESIZE_TOOLTIP.toString(), false);
+        addClassButton = initChildButton(fileToolbarPane, PropertyType.ADDCLASS_ICON.toString(), PropertyType.ADDCLASS_TOOLTIP.toString(), false);
+        addInterfaceButton = initChildButton(fileToolbarPane, PropertyType.ADDINTERFACE_ICON.toString(), PropertyType.ADDINTERFACE_TOOLTIP.toString(), false);
+        removeButton = initChildButton(fileToolbarPane, PropertyType.REMOVE_ICON.toString(), PropertyType.REMOVE_TOOLTIP.toString(), false);
+        undoButton = initChildButton(fileToolbarPane, PropertyType.UNDO_ICON.toString(), PropertyType.UNDO_TOOLTIP.toString(), false);
+        redoButton = initChildButton(fileToolbarPane, PropertyType.REDO_ICON.toString(), PropertyType.REDO_TOOLTIP.toString(), false);
+        zoominButton = initChildButton(fileToolbarPane, PropertyType.ZOOMIN_ICON.toString(), PropertyType.ZOOMIN_TOOLTIP.toString(), false);
+        zoomoutButton = initChildButton(fileToolbarPane, PropertyType.ZOOMOUT_ICON.toString(), PropertyType.ZOOMOUT_TOOLTIP.toString(), false);
+      
+         
         classPane = new HBox();
         
+        Label className = new Label("Class Name:");
+        className.setFont(Font.font("Arial", 10));
+        classPane.getChildren().add(className);
         
-        //MAKE A LABEL
+        classPane.getChildren().add(dummy);
         
-
-	
-         leftPane.getChildren().add(classPane);
+        rightPane.getChildren().add(classPane);
         
-         packagePane = new HBox();
+        
+        packagePane = new HBox();
      
+        Label packageName = new Label("Package Name:");
+        packageName.setFont(Font.font("Arial", 10));
+        packagePane.getChildren().add(packageName);
+        
+        packagePane.getChildren().add(dummyData);
+        
+        rightPane.getChildren().add(packagePane);
+        
     
-    
-         leftPane.getChildren().add(packagePane);
-    
-          //ParentPane
-    
-    
-     
-        variables = new VBox();
-     
-        Label backc = new Label("Background Color");
-        backc.setFont(Font.font("Verdana", 20));
-        variables.getChildren().add(backc);
-    
-    
-    
-        leftPane.getChildren().add(variables);
-     
-        methods = new VBox();
-    
-        Label fillerc = new Label("Filler Color");
-        fillerc.setFont(Font.font("Arial", 20));
-        methods.getChildren().add(fillerc);
-    
-        leftPane.getChildren().add(methods);
-    
+        parentPane = new HBox();
+        
+        Label parentName = new Label("Parent: ");
+        parentName.setFont(Font.font("Arial", 10));
+        parentPane.getChildren().add(parentName);
+        
+        final ComboBox emailComboBox = new ComboBox();
+        emailComboBox.getItems().addAll(
+            "jacob.smith@example.com",
+            "isabella.johnson@example.com",
+            "ethan.williams@example.com",
+            "emma.jones@example.com",
+            "michael.brown@example.com"  
+        );
+        parentPane.getChildren().add(emailComboBox);
+        
+        rightPane.getChildren().add(parentPane);
+        
+       
+        variablePane = new HBox();
+        
+        Label variableName = new Label("Variables: ");
+        variableName.setFont(Font.font("Arial", 10));
+        variablePane.getChildren().add(variableName);
+        
+        addButton = initChildButton(variablePane, PropertyType.ADD_ICON.toString(), PropertyType.ADD_TOOLTIP.toString(), false);
+        minusButton = initChildButton(variablePane, PropertyType.MINUS_ICON.toString(), PropertyType.MINUS_TOOLTIP.toString(), false);
+        
+        rightPane.getChildren().add(variablePane);
+        
+      
+        variableTablePane = new StackPane();
+        table1.setEditable(true);
+        
+        TableColumn NameCol1 = new TableColumn("Name");
+        TableColumn TypeCol1 = new TableColumn("Type");
+        TableColumn StaticCol1 = new TableColumn("Static");
+        TableColumn AccessCol1 = new TableColumn("Access");
+        
+        table1.getColumns().addAll(NameCol1, TypeCol1, StaticCol1, AccessCol1);
+        variableTablePane.getChildren().add(table1);
+        rightPane.getChildren().add(variableTablePane);
+        
+        methodPane = new HBox();
+        
+        Label methodName = new Label("Methods: ");
+        methodName.setFont(Font.font("Arial", 10));
+        methodPane.getChildren().add(methodName);
+        
+        addMButton = initChildButton(methodPane, PropertyType.ADD_ICON.toString(), PropertyType.ADD_TOOLTIP.toString(), false);
+        minusMButton = initChildButton(methodPane, PropertyType.MINUS_ICON.toString(), PropertyType.MINUS_TOOLTIP.toString(), false);
+        
+        rightPane.getChildren().add(methodPane);
+        
+      
+        methodTablePane = new StackPane();
+        table2.setEditable(true);
+        
+        TableColumn NameCol2 = new TableColumn("Name");
+        TableColumn ReturnCol2 = new TableColumn("Return");
+        TableColumn StaticCol2 = new TableColumn("Static");
+        TableColumn AbstractCol2 = new TableColumn("Abstract");
+        TableColumn AccessCol2 = new TableColumn("Access");
+        TableColumn Arg1 = new TableColumn("Arg1");
+        TableColumn Arg2 = new TableColumn("Arg2");
+        
+        table2.getColumns().addAll(NameCol2, ReturnCol2, StaticCol2, AbstractCol2, AccessCol2,
+                Arg1, Arg2);
+        
+        methodTablePane.getChildren().add(table2);
+        rightPane.getChildren().add(methodTablePane);
+        
     /*
     //ADD CAMERA BUTTON
-    camera = new HBox();
-    
-    camera_button = initChildButton(camera, PropertyType.CAMERA.toString(), PropertyType.CAMERA.toString(), false);
-       
-    camera_button.setOnAction(e -> {
+    cameraButton.setOnAction(e -> {
 	try {
                 PropertiesManager props = PropertiesManager.getPropertiesManager();
                 FileChooser fc = new FileChooser();
@@ -206,9 +280,11 @@ public class Workspace extends AppWorkspaceComponent {
     
      leftPane.getChildren().add(camera);
         */
-     ((BorderPane) workspace).setRight(leftPane); //set the leftPane to the left of the BorderPane
-     ((BorderPane) workspace).setCenter(rightPane);
+    
+    ((BorderPane) workspace).setCenter(leftPane); //set the leftPane to the left of the BorderPane
+    ((BorderPane) workspace).setRight(rightPane); 
     }
+    
     /*
     private Rectangle getNewRectangle() {
         Rectangle r = new Rectangle();
@@ -270,12 +346,12 @@ public class Workspace extends AppWorkspaceComponent {
     }
     */
     public void initStyle(){
-       leftPane.getStyleClass().add("max_pane");
+       rightPane.getStyleClass().add("max_pane");
        classPane.getStyleClass().add("bordered_pane");
        packagePane.getStyleClass().add("bordered_pane");
        //parentPane.getStyleClass().add("bordered_pane");
-       variables.getStyleClass().add("bordered_pane");
-       methods.getStyleClass().add("bordered_pane");
+       variablePane.getStyleClass().add("bordered_pane");
+       methodPane.getStyleClass().add("bordered_pane");
     }
     
     public Button initChildButton(Pane toolbar, String icon, String tooltip, boolean disabled) {
@@ -302,7 +378,7 @@ public class Workspace extends AppWorkspaceComponent {
     
       
     public void resetWorkspace(){
-        rightPane.getChildren().clear();
+      //  rightPane.getChildren().clear(); //not correct
     }
 
     /**
@@ -311,15 +387,17 @@ public class Workspace extends AppWorkspaceComponent {
      */
     @Override
     public void reloadWorkspace() {
+        /*
         DataManager expo = (DataManager) app.getDataComponent();
         rightPane.getChildren().clear();
        for(Shape s : expo.getShapeList()){
            //createListener(s);
            //currentShape = s;
-            rightPane.getChildren().add(s);
+         //   rightPane.getChildren().add(s);
         }
                 
         rightPane.setCursor(Cursor.DEFAULT);
         selectedButton = null; //bugger
+*/
     }
 }
